5060ce0517d6eb848b6088d1e8787155813d104a
Rick Lim <rick.jason.a.lim@gmail.com>
2019-10-23 00:07:54 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,36 @@
-def addition(num1, num2)
 
+def addition(num1, num2)
+puts num1 + num2
 end
+
+addition(5, 4)
 
 def subtraction(num1, num2)
-
+puts num2 - num1
 end
+
+subtraction(5, 10)
 
 def division(num1, num2)
-
+puts num2 / num1
 end
+
+division(2, 50)
 
 def multiplication(num1, num2)
-
+puts num1 * num2
 end
+
+multiplication(4,30)
 
 def modulo(num1, num2)
-
+puts num2.modulo(num1)
 end
+
+modulo(5, 34)
 
 def square_root(num)
-
+puts Math.sqrt(num)
 end
+
+square_root(81)
